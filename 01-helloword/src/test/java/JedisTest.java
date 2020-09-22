1e250bb3440668d87e4ffd31e7265069acb3816e
yaoyao <905440208@qq.com>
2020-09-22 14:07:06 +0800

    获取hash中 所有的field value

--- 01-helloword/src/test/java/JedisTest.java
+++ 01-helloword/src/test/java/JedisTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
 
+import java.util.Map;
+
 /**
  * 客户端连接redis 测试类
  *
@@ -11,11 +13,22 @@ public class JedisTest {
     @Test
     public void testSet() {
         //1、连接redis
-        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        Jedis jedis = new Jedis("10.128.123.108", 6379);
         //2、操作redis
         jedis.set("name", "jedis-test");
         String name = jedis.get("name");
         System.out.println("name:" + name);
+
+        //指定数据库
+        jedis.select(2);
+        String name2 = jedis.get("atomicNumberTest-20200517");
+        System.out.println("name2:" + name2);
+
+        //获取hash中 所有的field value
+        jedis.select(4);
+        Map<String, String> result = jedis.hgetAll("SNOW_FLAKE_SERIAL_CODE");
+        System.out.println("result:" + result);
+
         //3、关闭连接
         jedis.close();
     }
